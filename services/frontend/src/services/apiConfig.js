ab06829f39c92e93c83e2841d30010ad82c2a675
Sunny Sinha <thesunnysinha@gmail.com>
2025-01-15 22:10:34 +0000

    backend ip update

--- services/frontend/src/services/apiConfig.js
+++ services/frontend/src/services/apiConfig.js
@@ -1,11 +1,11 @@
-let env = "docker";
+let env = "prod";
 let API_BASE_URL;
 let CHANNEL_URL;
 
 if (env === "prod") {
   // Production environment
-  API_BASE_URL = "";
-  CHANNEL_URL = "";
+  API_BASE_URL = `http://172.105.43.160:80`;
+  CHANNEL_URL = `172.105.43.160:80`;
 } else {
     // Local environment
     API_BASE_URL = `http://127.0.0.1:8000`;
