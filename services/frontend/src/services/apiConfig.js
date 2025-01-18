e4512c0dd7db90e7cab9906c4863430fa0058bdc
Sunny Sinha <sunnysinha@Sunnys-MacBook-Air.local>
2025-01-18 23:55:27 +0530

    update server url

--- services/frontend/src/services/apiConfig.js
+++ services/frontend/src/services/apiConfig.js
@@ -4,8 +4,8 @@ let CHANNEL_URL;
 
 if (env === "prod") {
   // Production environment
-  API_BASE_URL = `https://172.105.49.244:443`;
-  CHANNEL_URL = `172.105.49.244:443`;
+  API_BASE_URL = `https://andocs.sunnysinha.space`;
+  CHANNEL_URL = `andocs.sunnysinha.space`;
 } else {
     // Local environment
     API_BASE_URL = `http://127.0.0.1:8000`;
