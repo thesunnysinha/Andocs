522f422390ef8fc2a07adc70f410e267bd225bb1
Sunny Sinha <sunnysinha@Sunnys-MacBook-Air.local>
2025-01-19 00:00:12 +0530

    update cors

--- services/backend/Andocs/settings.py
+++ services/backend/Andocs/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.User'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://https://andocs.vercel.app/',
+    'https://andocs.vercel.app/',
 ]
 
 PASSWORD_RESET_TIMEOUT = 900
